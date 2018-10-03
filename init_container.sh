1154b2ed48d4690fbd2af50a7105536921783d8f
yiwang88 <yiwang88@hotmail.com>
2018-10-03 11:12:35 -0500

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-service ssh start
+echo "Setup openrc ..." && openrc && touch /run/openrc/softlevel
+echo Starting ssh service...
+rc-service sshd start
+
 cp /opt/resolv.conf /etc/resolv.conf
 python /opt/app/main.py
